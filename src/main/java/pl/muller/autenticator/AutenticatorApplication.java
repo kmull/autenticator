198a5ee61a4b1f7776e31b747a3551fcaef9df89
krzys <krzysztofmuller1@gmail.com>
2020-12-29 16:21:32 +0100

    inicjalizacja aplikacji do autoryzacji tokenów

---
+++ src/main/java/pl/muller/autenticator/AutenticatorApplication.java
@@ -0,0 +1,13 @@
+package pl.muller.autenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/muller/autenticator/AutenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.muller.autenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
